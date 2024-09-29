4d6c46e0ab631171af2db4c76fc78efe54875198
noora amer <aoora6@gmail.com>
2024-09-29 10:13:00 +0300

    Initial commit

---
+++ project_1/project_1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  project_1
+//
+//  Created by Noori on 29/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ project_1/project_1/project_1App.swift
@@ -0,0 +1,17 @@
+//
+//  project_1App.swift
+//  project_1
+//
+//  Created by Noori on 29/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct project_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
