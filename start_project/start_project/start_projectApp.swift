3e71c12508b64a3ea6a2bbcc02a5153ac3ff81ea
noora amer <aoora6@gmail.com>
2024-10-01 11:13:15 +0300

    a fresh start

---
+++ start_project/start_project/start_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  start_projectApp.swift
+//  start_project
+//
+//  Created by Noori on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct start_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
