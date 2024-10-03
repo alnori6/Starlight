fbd50e90d43ddcb500e103eb6c16221252893298
Shaden14 <shadenabdullah14@gmail.com>
2024-10-03 10:16:40 +0300

    Sad Page

---
+++ start_project/start_project/Home Page.swift
@@ -0,0 +1,18 @@
+//
+//  Home Page.swift
+//  start_project
+//
+//  Created by Shaden Alhumood on 29/03/1446 AH.
+//
+
+import SwiftUI
+
+struct Home_Page: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home_Page()
+}
