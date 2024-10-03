f264bbf2ef13def6308332c37b2beb277c56b930
Shaden14 <shadenabdullah14@gmail.com>
2024-10-03 10:23:11 +0300

    Home Page Without function

--- start_project/start_project/ContentView.swift
+++ start_project/start_project/ContentView.swift
@@ -8,45 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
+        
+        
         ZStack {
-                
-                Image("background2")
-                    .resizable()
-                    .ignoresSafeArea()
-                    .frame(width: UIScreen.main.bounds.width,height: UIScreen.main.bounds.height)
-                
+            
+            Image("background2")
+                .resizable()
+                .ignoresSafeArea()
+                .frame(width: UIScreen.main.bounds.width,height: UIScreen.main.bounds.height)
+            
             VStack{
                 Text("Calm")
                     .font(.largeTitle)
-                    .font(.custom("SFProRounded", size: 30))
-               // .font(.system(size: 16))
+                    .font(Font.custom("SFProRounded-Bold", size: 16))
+                // .font(.system(size: 16))
                     .fontWeight(.bold)
                     .foregroundColor(Color(red: 1.0, green: 0.9882352941176471, blue: 0.9686274509803922))
                     .multilineTextAlignment(.center)
                     .offset( y: -5)
                 
                 
-        ZStack{
+                ZStack{
                     RoundedRectangle(cornerRadius: 20)
                         .frame(width: 333, height: 299)
                         .offset(x: -1, y: 80)
                         .foregroundColor(/*@START_MENU_TOKEN@*/Color(red: 0.9215686274509803, green: 0.8980392156862745, blue: 0.9019607843137255)/*@END_MENU_TOKEN@*/)
                         .padding()
                     
-            Image("small_happy")
-                
-                .offset(x: -1, y: 10)
-             
-            Text("Is a state of tranquility and peace, free from stress and anxiety. It fosters clear thinking and emotional balance, enhancing well-being and focus. ")
-                .font(.system(size: 16))
-                .fontWeight(.medium)
-                .foregroundColor(Color(red: 0.24705882352941178, green: 0.29411764705882354, blue: 0.4980392156862745))
-                .multilineTextAlignment(.center)
-                .offset(x: -1, y: 130)
-                .padding(.horizontal, 40)
-            
-                    }
+                    Image("small_happy")
+                    
+                        .offset(x: -1, y: 10)
+                    
+                    Text("Is a state of tranquility and peace, free from stress and anxiety. It fosters clear thinking and emotional balance, enhancing well-being and focus. ")
+                        .font(.system(size: 16))
+                        .fontWeight(.medium)
+                        .foregroundColor(Color(red: 0.24705882352941178, green: 0.29411764705882354, blue: 0.4980392156862745))
+                        .multilineTextAlignment(.center)
+                        .offset(x: -1, y: 130)
+                        .padding(.horizontal, 40)
+                    
+                }
                 
                 
                 ZStack{
@@ -66,56 +69,49 @@ struct ContentView: View {
                         .offset(x: -1, y: 15)
                         .padding(.horizontal, 30)
                     
-                HStack{
-                          Image("Path")
-                          .offset(x: 0, y: 50)
-                          Text("listen to music")
-                          .font(.system(size: 14))
-                          .fontWeight(.medium)
-                          .foregroundColor(Color(red: 0.24705882352941178, green: 0.29411764705882354, blue: 0.4980392156862745))
-                          .multilineTextAlignment(.leading)
-                           .offset(x:0, y: 50)
-            
-                    Image("Path")
-                    .offset(x: 5, y: 50)
-                    
-                    Text("read a book")
-                    .font(.system(size: 16))
-                    .fontWeight(.medium)
-                    .foregroundColor(Color(red: 0.24705882352941178, green: 0.29411764705882354, blue: 0.4980392156862745))
-                    .multilineTextAlignment(.leading)
-                     .offset(x: 5, y: 50)
-            
+                    HStack{
+                        Image("Path")
+                            .offset(x: 0, y: 50)
+                        Text("listen to music")
+                            .font(.system(size: 14))
+                            .fontWeight(.medium)
+                            .foregroundColor(Color(red: 0.24705882352941178, green: 0.29411764705882354, blue: 0.4980392156862745))
+                            .multilineTextAlignment(.leading)
+                            .offset(x:0, y: 50)
+                        
+                        Image("Path")
+                            .offset(x: 5, y: 50)
+                        
+                        Text("read a book")
+                            .font(.system(size: 16))
+                            .fontWeight(.medium)
+                            .foregroundColor(Color(red: 0.24705882352941178, green: 0.29411764705882354, blue: 0.4980392156862745))
+                            .multilineTextAlignment(.leading)
+                            .offset(x: 5, y: 50)
+                        
                         
                     }
                     HStack{
                         
                         Image("Path")
-                        .offset(x: 0, y: 70)
+                            .offset(x: 0, y: 70)
                         Text("go for a walk")
-                        .font(.system(size: 16))
-                        .fontWeight(.medium)
-                        .foregroundColor(Color(red: 0.24705882352941178, green: 0.29411764705882354, blue: 0.4980392156862745))
-                        .multilineTextAlignment(.leading)
-                         .offset(x: -0, y: 70)
-          
-                  Image("Path")
-                  .offset(x: 5, y: 70)
-                  
-                  Text("call a friend")
-                  .font(.system(size: 16))
-                  .fontWeight(.medium)
-                  .foregroundColor(Color(red: 0.24705882352941178, green: 0.29411764705882354, blue: 0.4980392156862745))
-                  .multilineTextAlignment(.leading)
-                   .offset(x: 5, y: 70)
-          
+                            .font(.system(size: 16))
+                            .fontWeight(.medium)
+                            .foregroundColor(Color(red: 0.24705882352941178, green: 0.29411764705882354, blue: 0.4980392156862745))
+                            .multilineTextAlignment(.leading)
+                            .offset(x: -0, y: 70)
                         
+                        Image("Path")
+                            .offset(x: 5, y: 70)
                         
-                        
-                        
-                        
-                        
-                    }
+                        Text("call a friend")
+                            .font(.system(size: 16))
+                            .fontWeight(.medium)
+                            .foregroundColor(Color(red: 0.24705882352941178, green: 0.29411764705882354, blue: 0.4980392156862745))
+                            .multilineTextAlignment(.leading)
+                            .offset(x: 5, y: 70)
+                   }
                     
                     
                     
@@ -131,13 +127,14 @@ struct ContentView: View {
                 
                 
                 
-                    }
-
-                   
-                }
-                    
-                }
             }
+            
+            
+        }
+        
+    }
+  
+}
 
 
 
--- start_project/start_project/Emotion 4.swift
+++ start_project/start_project/Emotion 4.swift
@@ -9,9 +9,138 @@ import SwiftUI
 
 struct Emotion_4: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        ZStack {
+                
+                Image("background2")
+                    .resizable()
+                    .ignoresSafeArea()
+                    .frame(width: UIScreen.main.bounds.width,height: UIScreen.main.bounds.height)
+                
+            VStack{
+                Text("Angry")
+                    .font(.largeTitle)
+                    .font(.custom("SFProRounded", size: 30))
+               // .font(.system(size: 16))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(red: 1.0, green: 0.9882352941176471, blue: 0.9686274509803922))
+                    .multilineTextAlignment(.center)
+                    .offset( y: -5)
+                
+                
+        ZStack{
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 333, height: 299)
+                        .offset(x: -1, y: 80)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/Color(red: 0.9215686274509803, green: 0.8980392156862745, blue: 0.9019607843137255)/*@END_MENU_TOKEN@*/)
+                        .padding()
+                    
+            Image("small_angry 1")
+                
+                .offset(x: -1, y: 10)
+             
+            Text("Is an emotional response to frustration and injustice, often stemming from feelings of powerlessness.  ")
+                .font(.system(size: 16))
+                .fontWeight(.medium)
+                .foregroundColor(Color(red: 0.24705882352941178, green: 0.29411764705882354, blue: 0.4980392156862745))
+                .multilineTextAlignment(.center)
+                .offset(x: -1, y: 130)
+                .padding(.horizontal, 40)
+            
+                    }
+                
+                
+                ZStack{
+                    
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 333, height: 125)
+                        .offset(x: -1, y: 55)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/Color(red: 0.9215686274509803, green: 0.8980392156862745, blue: 0.9019607843137255)/*@END_MENU_TOKEN@*/)
+                        .padding()
+                    
+                    Text("Ways to make your life more pleasurable: ")
+                    
+                        .font(.system(size: 14))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(red: 0.24705882352941178, green: 0.29411764705882354, blue: 0.4980392156862745))
+                        .multilineTextAlignment(.center)
+                        .offset(x: -1, y: 15)
+                        .padding(.horizontal, 30)
+                    
+                HStack{
+                          Image("Path")
+                          .offset(x: 0, y: 50)
+                          Text("listen to music")
+                          .font(.system(size: 14))
+                          .fontWeight(.medium)
+                          .foregroundColor(Color(red: 0.24705882352941178, green: 0.29411764705882354, blue: 0.4980392156862745))
+                          .multilineTextAlignment(.leading)
+                           .offset(x:0, y: 50)
+            
+                    Image("Path")
+                    .offset(x: 5, y: 50)
+                    
+                    Text("read a book")
+                    .font(.system(size: 16))
+                    .fontWeight(.medium)
+                    .foregroundColor(Color(red: 0.24705882352941178, green: 0.29411764705882354, blue: 0.4980392156862745))
+                    .multilineTextAlignment(.leading)
+                     .offset(x: 5, y: 50)
+            
+                        
+                    }
+                    HStack{
+                        
+                        Image("Path")
+                        .offset(x: 0, y: 70)
+                        Text("go for a walk")
+                        .font(.system(size: 16))
+                        .fontWeight(.medium)
+                        .foregroundColor(Color(red: 0.24705882352941178, green: 0.29411764705882354, blue: 0.4980392156862745))
+                        .multilineTextAlignment(.leading)
+                         .offset(x: -0, y: 70)
+          
+                  Image("Path")
+                  .offset(x: 5, y: 70)
+                  
+                  Text("call a friend")
+                  .font(.system(size: 16))
+                  .fontWeight(.medium)
+                  .foregroundColor(Color(red: 0.24705882352941178, green: 0.29411764705882354, blue: 0.4980392156862745))
+                  .multilineTextAlignment(.leading)
+                   .offset(x: 5, y: 70)
+          
+                        
+                        
+                        
+                        
+                        
+                        
+                    }
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                }
+                
+                
+                
+                
+                    }
+
+                   
+                }
+                    
+                }
+            }
+
+
+
 
 #Preview {
     Emotion_4()
