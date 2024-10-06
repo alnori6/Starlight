5a58bb5e9d7cce9ad8013c9443b98f7383b4af37
Shaden14 <shadenabdullah14@gmail.com>
2024-10-06 11:52:33 +0300

    ي

--- start_project/start_project/Home Page.swift
+++ start_project/start_project/Home Page.swift
@@ -8,14 +8,6 @@
 import SwiftUI
 
 struct Home_Page: View {
-    var body: some View {
-        
-        ZStack{
-           
-            Image("Home page")
-               .resizable()
-                .ignoresSafeArea()
-
 var body: some View {
     
     ZStack{
